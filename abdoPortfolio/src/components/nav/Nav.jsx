3e10cdeca5bffdefe313a744999d0bdb14ade967
Abdo Badr <abdelraheman.badr.2005@gmail.com>
2026-09-03 02:35:37 +0300

    add Nav component with navigation links and active state; update CSS for responsive design and styling

--- abdoPortfolio/src/components/nav/Nav.jsx
+++ abdoPortfolio/src/components/nav/Nav.jsx
@@ -1,9 +1,24 @@
 import './nav.css'
+import { IoHomeOutline } from "react-icons/io5";
+import { LuUserRound } from "react-icons/lu";
+import { BiBook } from "react-icons/bi";
+import { RiServiceLine } from "react-icons/ri";
+import { BiMessageSquareDetail } from "react-icons/bi";
+import { useState } from 'react';
+
 
 function Nav() {
+    const [activeNav , setActive] = useState('#')
     return (
-        <div>
-        </div>
+        <>
+        <nav>
+            <a href="#" className={activeNav === "#" ? "active" : ""} onClick={() => setActive('#')}><IoHomeOutline /></a>
+            <a href="#about" className={activeNav === "#about" ? "active" : ""} onClick={() => setActive('#about')}><LuUserRound /></a>
+            <a href="#services" className={activeNav === "#services" ? "active" : ""} onClick={() => setActive('#services')}><BiBook /></a>
+            <a href="#project" className={activeNav === "#project" ? "active" : ""} onClick={() => setActive('#project')}><RiServiceLine /></a>
+            <a href="#contact" className={activeNav === "#contact" ? "active" : ""} onClick={() => setActive('#contact')}><BiMessageSquareDetail /></a>
+        </nav>
+        </>
         )
     }
 
